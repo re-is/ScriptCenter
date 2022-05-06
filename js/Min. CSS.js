cbae2fd576acd7a3ecac290dafdc3a79b7347ece
re-is <33784291+re-is@users.noreply.github.com>
2022-05-06 20:17:01 +0200

    Update Min. CSS.js

--- js/Min. CSS.js
+++ js/Min. CSS.js
@@ -2,124 +2,42 @@
 	*** CSS Minifier ***
 
 	Author: István Reich
-	Release date: 2018.06.27.
+	Release date: 2022.05.06.
 -------------------------------*/
 
 (function() {
 
+	//-----------------------------------------------------+
+	var css_file = 'shl.css';
+	//-----------------------------------------------------+
 	var fso = new ActiveXObject('Scripting.FileSystemObject');
-
-	//-----------------------------------------------------+
-	var css_file = 'Red Line YouTube.css';
-	//-----------------------------------------------------+
-
 	var opened_text_file = fso.OpenTextFile(css_file, 1);
-	var opened_text = opened_text_file.ReadAll(); opened_text_file.Close();
-	var char = opened_text.split("");
-	var text = "";
-	var activated = "";
-	var string_char = false;
-	var string_single = false;
-	var string_double = false;
-	var string_regex = false;
-	var comment_block = false;
-	var bracket_block = false;
-	var new_line = true;
+	var css_text = opened_text_file.ReadAll(); opened_text_file.Close();
 
-	function $(c, r) {
-		if (c === undefined) return false;
-		return (c.search(r) == 0);
+	// Kommentek törlése: ( HTML, CSS, Js )
+	css_text = css_text.replace(/\r\n/g, '').replace(/(\/\*.*?\*\/)/g, '');
+
+	var double = css_text.split('"'), double_text = '';
+	for (var i = 0; i < double.length; i++) {
+		double_text += (i % 2 ? ('"' + double[i].replace(/\s/g, 'a(------(space)------)a') + '"') : double[i]);
 	}
 
-	// Karakterekre bontás:
-	for (var i = 0; i < char.length; i++) {
-		// Ha nincs előtte \ jel:
-		if (!string_char) {
-			// Single
-			if ($(char[i], /\'/)) {
-				if (activated == "") {
-					string_single = true;
-					activated = "single";
-				}
-				else if (activated == "single") activated = "";
-			}
-			// Double
-			if ($(char[i], /\"/)) {
-				if (activated == "") {
-					string_double = true;
-					activated = "double";
-				}
-				else if (activated == "double") activated = "";
-			}
-			//		/ jel után:
-			if ($(char[i], /\//)) {
-				// Block kezdés, ha a következő karakter *
-				//		/*
-				if ($(char[i+1], /\*/)) {
-					if (activated == "") {
-						comment_block = true;
-						activated = "block";
-					}
-				}
-				// Block vége, ha az előző karakter *
-				//		*/
-				if ($(char[i-1], /\*/) && (activated == "block")) activated = "";
-			}
-		}
-		// Műveletek kommenten kívül:
-		if (!comment_block) {
-			// Új sor:
-			if ($(char[i], /\n/)) new_line = true;
-			// Amíg a szóköz van:
-			if ($(char[i], /\S/)) new_line = false;
-			// Sor eleji szóközök törlése:
-			if (new_line) char[i] = "";
-			// Egysorba rakás:
-			if ($(char[i], /\r/)) char[i] = "";
-			// --------------------------------------------+
-			// string-en kívül:
-			if ((!string_single) && (!string_double)) {
-				// Kapocs nyitás:
-				if ($(char[i], /\{/)) bracket_block = true;
-				// Kapocs zárás:
-				if ($(char[i], /\}/)) {
-					bracket_block = false;
-					char[i] = "semicolon_before_bracket\}";
-				}
-				// Szóközök törlése a operátorok elől és mögül:
-				if ($(char[i], /\s/) && bracket_block) {
-					if (!$(char[i+1], /\#/) && ($(char[i-1], /\B/) || $(char[i+1], /\B/) || ($(char[i-1], /\b/) && $(char[i+1], /\B/)) || ($(char[i-1], /\B/) && $(char[i+1], /\b/)))) char[i] = "";
-					// : # => :#
-					if ($(char[i-1], /\:/) && $(char[i+1], /\#/)) char[i] = "";
-				}
-			}
-			// string-en belül:
-			else {
-				// Sor végi per jelek törlése:
-				if ($(char[i], /\\/) && $(char[i+1], /\r/)) char[i] = "";
-				// Reguláris karakter indítása:
-				if ($(char[i], /\\/) && !string_char) string_char = true;
-				// Befejezése:
-				else if (string_char) string_char = false;
-			}
-			// StyleSheet írása:
-			text += char[i];
-		}
-
-		// Reset:
-		if (activated == "") {
-			if (string_single) string_single = false;
-			if (string_double) string_double = false;
-			if (string_regex) string_regex = false;
-			if (comment_block) comment_block = false;
-		}
+	var simple = double_text.split("'"), new_text = '';
+	for (var i = 0; i < simple.length; i++) {
+		new_text += (i % 2 ? ("'" + simple[i].replace(/\s/g, 'a(------(space)------)a') + "'") : simple[i]);
 	}
 
-	// ;} cseréje:
-	text = text.replace(/\;semicolon_before_bracket\}/g, "\}");
+	// Szóközök törlése:
+	new_text = new_text.replace(/((\s)\B|\B(\s))/g, '');
+
+	// Fontos szóközök visszaállítása:
+	new_text = new_text.replace(/a\(------\(space\)------\)a/g, ' ');
+
+	// ;}
+	new_text = new_text.replace(/\;\}/g, '}');
 
 	var tf = fso.CreateTextFile(css_file.substring(0, css_file.length - 4) + '.min.css', true);
-	tf.WriteLine(text);
+	tf.WriteLine(new_text);
 	tf.Close();
 
 })();
