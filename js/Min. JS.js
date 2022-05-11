5ae237d7844b14791e4e27aac385924547cb663c
re-is <33784291+re-is@users.noreply.github.com>
2022-05-11 20:08:55 +0200

    Update Min. JS.js

--- js/Min. JS.js
+++ js/Min. JS.js
@@ -1,46 +1,65 @@
-(function() {
+function js_min(js_name) {
 
-	//-----------------------------------------------------+
-	var js_name = 'teszt';
 	var js_file = (js_name + '.js');
-	//-----------------------------------------------------+
-
 	var fso = new ActiveXObject('Scripting.FileSystemObject');
 	var opened_text_file = fso.OpenTextFile(js_file, 1);
 	var text = opened_text_file.ReadAll(); opened_text_file.Close();
 	var ch = text.split('');
 	var js_text = '';
-	var string = '';
+	var string = '', string_temp = '';
+	var comment = '';
 
 	for (var i = 0; i < ch.length; i++) {
 
-		// String-en kívül:
-		if (string === '') {
-			js_text += ch[i];
-		}
-		else {
-			// /*
-			if (ch[i] === '/' && ch[i+1] === '*')			js_text += '@@_COMMENT_@@';
-			// */
-			else if (ch[i] === '*' && ch[i+1] === '/')		js_text += '@@_COMMENT_END_@@';
-			// //
-			else if (ch[i] === '/' && ch[i+1] === '/')		js_text += '@@_COMMENT_@@';
-			// space
-			else if (ch[i] === ' ')							js_text += '@@_SPACE_@@';
-			// tab
-			else if (ch[i] === '	')						js_text += '@@_TABULATOR_@@';
-			// ;}
-			else if (ch[i] === ';' && ch[i+1] === '}')		js_text += '@@_SEMICOLON_@@';
-			// minden más
-			else											js_text += ch[i];
+		if (comment === '' && string === '') {
+			if (ch[i] === '"' || ch[i] === "'" || (ch[i-1] === '(' && ch[i] === '/')) string_temp = ch[i];
 		}
 
-		if (string === '' && (ch[i] === '"' || ch[i] === "'" || (ch[i-1] === '(' && ch[i] === '/'))) string = ch[i];
-		else if (string === ch[i]) string = '';
+		if (comment === '' && string === '') {
+			if (ch[i] === '/' && ch[i+1] === '*') comment = 'block';
+			if (ch[i] === '/' && ch[i+1] === '/') comment = 'line';
+		}
+
+		// Kommenten kívül:
+		if (comment === '') {
+
+			// String-en kívül:
+			if (string === '') {
+				js_text += ch[i];
+			}
+			else {
+				// /*
+				if (ch[i] === '/' && ch[i+1] === '*')			js_text += '@@_COMMENT_@@';
+				// *-/
+				else if (ch[i] === '*' && ch[i+1] === '/')		js_text += '@@_COMMENT_END_@@';
+				// //
+				else if (ch[i] === '/' && ch[i+1] === '/')		js_text += '@@_COMMENT_@@';
+				// space
+				else if (ch[i] === ' ')							js_text += '@@_SPACE_@@';
+				// tab
+				else if (ch[i] === '	')						js_text += '@@_TABULATOR_@@';
+				// ;}
+				else if (ch[i] === ';' && ch[i+1] === '}')		js_text += '@@_SEMICOLON_@@';
+				// minden más
+				else											js_text += ch[i];
+			}
+
+		}
+
+		if (comment === 'block' && ch[i-1] === '*' && ch[i] === '/') comment = '';
+		if (comment === 'line' && ch[i+1] === '\n') comment = '';
+
+		// Először még nincs megadva a string, mert azonnal kikapcsolná:
+		if (string === ch[i]) {
+			string_temp = '';
+			string = '';
+		}
+		// A következő karakteren kell:
+		else string = string_temp;
 	}
 
 	// Comment sorok:
-	js_text = js_text.replace(/([/][/].*(?=\*\/))|([/][/].*)/g, '\r\n');
+	js_text = js_text.replace(/(\/\/.*(?=\*\/))|(\/\/.*)/g, '\r\n');
 
 	// TAB:
 	js_text = js_text.replace(/\t/g, '');
@@ -49,13 +68,13 @@
 	js_text = js_text.replace(/\r*\n*/g, '');
 
 	// Comment block:
-	js_text = js_text.replace(/([/][*].*[*][/])/g, '');
+	js_text = js_text.replace(/(\/\*.*\*\/)/g, '');
 
 	// Szóközök
 	js_text = js_text.replace(/(\s\B)|(\B\s)/g, '');
 
 	// ;}
-	js_text = js_text.replace(/[;][}]/g, '}');
+	js_text = js_text.replace(/(\;\})/g, '}');
 
 	// String-en belüliek visszaállítása:
 	js_text = js_text.replace(/@@_COMMENT_@@/g, '/');
@@ -68,4 +87,8 @@
 	min.WriteLine(js_text);
 	min.Close();
 
-})();
+	return js_text;
+
+}
+
+js_min('teszt');
